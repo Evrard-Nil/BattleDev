be81c3de49b706df196d9fe8f45e7eddbba1d875
evrard-nil <evrard-nil.daillet@soprasteria.com>
2019-10-20 14:27:46 +0200

    Brute force solution

--- CA_2019/2-Chocolate_factory/solution.py
+++ CA_2019/2-Chocolate_factory/solution.py
@@ -1,4 +1,20 @@
 def solve(content):
     '''
+    Quadratic solution
+    int with the least number of digit leading to a sum of 42 is 99996
+    whenever the sum goes under this number we can stop.
+    TODO: find a less compex solution
     '''
-    return content
+    epochs = content[1:]
+    i = 0
+    for e in epochs:
+        if e == '42':
+            i += 1
+        else:
+            sums = int(e)
+            while sums > 99996:
+                sums = sum([int(s) for s in str(sums)])
+                if sums == 42:
+                    i += 1
+                    break
+    return i
